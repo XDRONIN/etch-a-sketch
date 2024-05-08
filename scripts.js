9b7438d654b1556cfef68e5ffb312e9cb01fd792
XDRONIN <akashprakashmattathil@gmail.com>
2024-05-08 12:03:06 +0530

    All the buttons are functional and the project is com[plete functionally

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let gridNum = 8;
+let mode = "pen";
 let container = document.getElementById("container");
 createGrid(gridNum);
 function createGrid(gridNum) {
@@ -10,6 +11,17 @@ function createGrid(gridNum) {
       let newDiv = document.createElement("div");
       newDiv.classList.add("newDiv");
       newRow.appendChild(newDiv);
+      newDiv.addEventListener("mouseover", () => {
+        if (mode == "pen") {
+          newDiv.style.backgroundColor = "black";
+        } else if (mode == "colour") {
+          let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+          newDiv.style.backgroundColor = `#${randomColor}`;
+        } else if (mode == "eraser") {
+          newDiv.style.backgroundColor = "";
+        }
+      });
     }
   }
 }
@@ -18,4 +30,17 @@ gridButt.addEventListener("click", () => {
   container.innerHTML = "";
   gridNum = prompt("Enter the number of grids you want between 1 and 100");
   createGrid(gridNum);
+});
+
+const pen = document.getElementById("pen");
+const colour = document.getElementById("colour");
+const eraser = document.getElementById("eraser");
+pen.addEventListener("click", () => {
+  mode = "pen";
+});
+colour.addEventListener("click", () => {
+  mode = "colour";
+});
+eraser.addEventListener("click", () => {
+  mode = "eraser";
 });
